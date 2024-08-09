f50074828df951c10cdac489741b2b429096f0e6
forgottencsc <forgottencosecant@outlook.com>
2024-08-09 20:28:40 +0800

    wip

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main(void) {
+graph_t read_simiple_graph(istream& is) {
     size_t n, m;
     cin >> n >> m;
 
@@ -19,11 +19,32 @@ int main(void) {
         v--;
         boost::add_edge(u, v, edge_property_t(w), g);
     }
+    return g;
+}
 
-    auto res = tsp(g);
+graph_t read_vlsi(istream& is) {
+    typedef pair<double, double> pdd;
+    vector<pdd> ps;
+    string line;
+    while (getline(is, line)) {
+        if (line.empty() || !isdigit(line[0])) continue;
+        int id;
+        pdd p;
+        sscanf(line.c_str(), "%d %lf %lf", &id, &p.first, &p.second);
+        ps.push_back(p);
+    }
+    const size_t n = ps.size();
+    graph_t g(n);
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = i + 1; j < n; ++j)
+            boost::add_edge(i, j, hypot(ps[i].first - ps[j].first, ps[i].second - ps[j].second), g);
+    return g;
+}
 
-
-    fmt::print("{}\n", res);
-    
-
+int main(void) {
+    ifstream ifs("/root/tspp/data/pjh17845.tsp");
+    const graph_t& g = read_vlsi(ifs);
+    const auto v = tspp(g, 0, 1);
+    fmt::print("{}", v);
+    return 0;
 }
--- tsp.hpp
+++ tsp.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "common.hpp"
-#include <boost/graph/metric_tsp_approx.hpp>
+
 vector<vertex_t> tsp(const graph_t& g) {
     const auto n = boost::num_vertices(g);
     vector<vertex_t> pre(n);
@@ -10,24 +10,108 @@ vector<vertex_t> tsp(const graph_t& g) {
 
     auto deg = calc_deg(n, tree_edges);
 
-    auto id = collect_odd_deg_vertices(deg);
+    auto ids = collect_odd_deg_vertices(deg);
 
-    graph_t h = induce(g, id);
+    graph_t h = induce(g, ids);
 
     vector<vertex_t> mate = minimum_weighted_matching(h);
 
-    auto matching_edges = restore(collect_matching_edges(mate), id);
+    auto matching_edges = restore(collect_matching_edges(mate), ids);
 
     vector<pvv> edges(move(tree_edges));
     edges.insert(edges.end(), matching_edges.begin(), matching_edges.end());
 
-    vector<vertex_t> eul_tour = eulerian_circuit(n, edges, 0);
+    vector<vertex_t> eul_tour = eulerian_path(n, edges, 0);
 
-    pair<weight_t, vector<vertex_t>> res(DBL_MAX, {});
-    for (size_t i = 0; i < edges.size(); ++i) {
-        vector<vertex_t> tsp_tour = filter_eul_tour(n, eul_tour);
-        rotate_eul_tour(eul_tour);
-        res = min(res, make_pair(tsp_tour_weight(g, tsp_tour), tsp_tour));
+    vector<bool> appeared(n, false);
+    vector<vertex_t> res;
+    for (vertex_t v : eul_tour) {
+        if (appeared[v]) continue;
+        appeared[v] = true;
+        res.push_back(v);
     }
-    return res.second;
+    return res;
+}
+
+vector<vertex_t> spp(const graph_t& g, const vector<vertex_t>& mate) {
+    const auto n = boost::num_vertices(g);
+    vector<vertex_t> pre(n);
+    auto new_weight = boost::make_function_property_map<graph_t::edge_descriptor, weight_t>([&](const graph_t::edge_descriptor& e) {
+        auto src = boost::source(e, g);
+        auto dst = boost::target(e, g);
+        auto w = boost::get(boost::edge_weight, g, e);
+        if (mate[src] == dst && mate[dst] == src)
+            return weight_t(0);
+        return w;
+    });
+    prim_minimum_spanning_tree(g, pre.data(), boost::weight_map(new_weight));
+
+
+    auto tree_edges = collect_tree_edges(pre);
+
+    auto deg = calc_deg(n, tree_edges);
+
+    auto id = collect_odd_deg_vertices(deg);
+
+    graph_t h = induce(g, id);
+
+    vector<vertex_t> mate2 = minimum_weighted_matching(h);
+    auto matching_edges = restore(collect_matching_edges(mate2), id);
+
+    vector<pvv> edges(move(tree_edges));
+    edges.insert(edges.end(), matching_edges.begin(), matching_edges.end());
+
+    vector<vertex_t> eul_tour = eulerian_path(n, edges, 0);
+
+    vector<bool> appeared(n, false);
+    vector<vertex_t> res;
+    for (size_t i = 0; i + 1 < eul_tour.size(); ++i) {
+        size_t u = eul_tour[i], v = eul_tour[i + 1];
+        if (appeared[u]) continue;
+        if (mate[u] != v) continue;
+        res.push_back(u);
+        res.push_back(v);
+        appeared[u] = appeared[v] = true;
+    }
+    vertex_t v = res.front();
+    res.push_back(v);
+
+    return res;
+}
+
+vector<vertex_t> tspp(const graph_t& g, vertex_t s, vertex_t t) {
+    const auto n = boost::num_vertices(g);
+    vector<vertex_t> pre(n);
+    prim_minimum_spanning_tree(g, pre.data());
+
+    auto tree_edges = collect_tree_edges(pre);
+
+    auto deg = calc_deg(n, tree_edges);
+
+    deg[s] ^= 1;
+    deg[t] ^= 1;
+
+    const auto ids = collect_odd_deg_vertices(deg);
+    
+    graph_t h = induce(g, ids);
+
+
+    vector<vertex_t> mate2 = minimum_weighted_matching(h);
+    auto matching_edges = restore(collect_matching_edges(mate2), ids);
+
+    vector<pvv> edges(move(tree_edges));
+    edges.insert(edges.end(), matching_edges.begin(), matching_edges.end());
+    deg = calc_deg(n, edges);
+    
+    vector<vertex_t> eul_tour = eulerian_path(n, edges, s);
+    vector<bool> appeared(n, false);
+    appeared[s] = true;
+    vector<vertex_t> res;
+    for (vertex_t v : eul_tour) {
+        if (appeared[v]) continue;
+        appeared[v] = true;
+        res.push_back(v);
+    }
+    res.push_back(s);
+    return res;
 }
